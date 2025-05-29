536a4084559085226b7fa96fb9d8b6254a3fcf33
KaterynaBlinova-1995 <catalina.ova2709@gmail.com>
2025-05-30 00:31:15 +0300

    all changes

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,11 @@
 function getElementWidth(content, padding, border) {
-  const Npx = content + padding * 2 + border * 2;
-  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+  const contentValue = parseFloat(content);
+  const paddingValue = parseFloat(padding) * 2;
+  const borderValue = parseFloat(border) * 2;
+
+  const totalWidth = contentValue + paddingValue + borderValue;
+
+  return totalWidth;
 }
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
